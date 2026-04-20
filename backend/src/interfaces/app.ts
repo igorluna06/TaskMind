db00bf59e16ea09ee307352ec26e07180162efd4
Igor Luna <igorluna2006@gmail.com>
2026-04-20 18:39:10 -0300

    feat: add CORS configuration for frontend dev server

--- backend/src/interfaces/app.ts
+++ backend/src/interfaces/app.ts
@@ -3,9 +3,12 @@ import { errorHandler } from "./middlewares/errorHandler";
 import eventRoutes from "./routes/event/eventRoutes";
 import { RoutePaths } from "./routes/constants/RoutePaths";
 import aiRoutes from "./routes/ai/aiRoutes";
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 app.use(express.json());
 app.use(RoutePaths.EVENTS, eventRoutes);
 app.use(RoutePaths.AI, aiRoutes);
